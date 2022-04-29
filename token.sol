f483ba15d46872aac918f574ec83247ef13f8aaa
Dragoelelele <kingdragox99@gmail.com>
2022-04-29 08:55:56 +0200

    Update token.sol

--- token.sol
+++ token.sol
@@ -5,7 +5,7 @@ import "../contracts/ptsFee.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PTS is ERC20FAB {
-    constructor() ERC20FAB("PickUpTaSoeur", "PERCE", 0x4E555a68E3C22144556dF067B03a42D571eED635, 2, 2) {
+    constructor() ERC20FAB("PickUpTaSoeur", "PTS", 0xd7E38327513Dcb44e9680Cdeb31AA59D3CA93CFC, 2, 2) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
